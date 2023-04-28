597a7d858e675bd4f53960905f9ba1a25f1282dd
GabrieliusSavickis <gabrrielius@gmail.com>
2023-04-28 13:17:22 +0000

    1.Updated Home page with additional buttons to the pages; could add some additional info like news to fill in the space 2. Created the structure for the APIS in F1; API has a bug not showing information

--- my-react-app/src/components/F1.js
+++ my-react-app/src/components/F1.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../styling/navbar.css';
+import '../styling/f1.css';
 
-export class F1 extends React.Component {
-    render() {
-        return <div className="F1">
+function F1() {
+
+    const [drivers, setDrivers] = useState([]);
+
+    useEffect(() => {
+        axios
+          .get('http://ergast.com/api/f1/2023/drivers')
+          .then(response => {
+            setDrivers(response.data.MRData.DriverTable.Drivers);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, []);
+
+    return (
+        <div className="F1">
             <div className="NavbarMenu">
-            <Navbar>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/Home">Home</Nav.Link>
-                    <Nav.Link href="/F1">Formula 1</Nav.Link>
-                    <Nav.Link href="/MotoGP">MotoGP</Nav.Link>
-                </Nav>
-            </Navbar>
+                <Navbar>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/Home">Home</Nav.Link>
+                        <Nav.Link href="/F1">Formula 1</Nav.Link>
+                        <Nav.Link href="/MotoGP">MotoGP</Nav.Link>
+                    </Nav>
+                </Navbar>
             </div>
-            <h1>This is the F1</h1>
+
+            <div className="grid-container">
+                <div className="item1">
+                    <h1>2023 Drivers Championship</h1>
+                    <ul>
+                        {drivers.map(driver => (
+                            <li key={driver.driverId}>
+                                {driver.givenName} {driver.familyName}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="item2">
+                    <h2>Constructors Championship</h2>
+                </div>
+                <div className="item3">
+                    <h2>Drivers</h2>
+                </div>
+                <div className="item4">
+                    <h2>Locations</h2>
+                </div>
+            </div>
         </div>
-    }
+    );
 }
+
+export default F1;
--- my-react-app/src/components/Home.js
+++ my-react-app/src/components/Home.js
@@ -2,24 +2,34 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../styling/navbar.css';
-
+import '../styling/button-container.css';
 
 function Home() {
 
-    //This hook returns the current location object
-    return (
-        <div className="NavbarMenu">
-            <Navbar>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/Home">Home</Nav.Link>
-                    <Nav.Link href="/F1">Formula 1</Nav.Link>
-                    <Nav.Link href="/MotoGP">MotoGP</Nav.Link>
-                </Nav>
-            </Navbar>
-            <h1>Hello and welcome to the home of MotorSports</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+  return (
+    <div className="NavbarMenu">
+      <Navbar>
+        <Nav className="mr-auto">
+          <Nav.Link href="/Home">Home</Nav.Link>
+          <Nav.Link href="/F1">Formula 1</Nav.Link>
+          <Nav.Link href="/MotoGP">MotoGP</Nav.Link>
+        </Nav>
+      </Navbar>
+      <div className="Homepage">
+        <h1>Hello and welcome to the home of MotorSports</h1>
+        <div className="button-container">
+          <a href="/F1" className="button">Go to F1</a>
+          <a href="/MotoGP" className="button">Go to MotoGP</a>
         </div>
-    );
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
