f52cb4872c9bce72c9bc3001ba171ededfb989c4
Kaif Tahir <79585814+Kaif370Z@users.noreply.github.com>
2023-04-28 13:03:54 +0000

    MotoGP and F1 Api Still work in Progress But Very close to working

--- my-react-app/src/components/MotoGP.js
+++ my-react-app/src/components/MotoGP.js
@@ -1,64 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import '../styling/navbar.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import React, { useState, useEffect } from 'react';
 
-
-
-const options = {
-  method: 'POST',
-  url: 'https://motogp2.p.rapidapi.com/api/data/get_race_details_of_season',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'd5f52960c8mshe65ce0ba97ab492p19e6a5jsn5041f65a619f',
-    'X-RapidAPI-Host': 'motogp2.p.rapidapi.com'
-  },
-  data: {
-    season_id: 'db8dc197-c7b2-4c1b-b3a4-6dc534c023ef'
-  }
-};
-
-function MotoGP() {
-
-  const [races, setRaces] = useState([]);
+const MotoGP = () => {
+  const [statistics, setStatistics] = useState(null);
 
   useEffect(() => {
-    async function fetchRaces() {
+    const getStatistics = async () => {
+      const url = 'https://motogp2.p.rapidapi.com/api/data/get_rider_statistics_from_id';
+      const options = {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': 'd5f52960c8mshe65ce0ba97ab492p19e6a5jsn5041f65a619f',
+          'X-RapidAPI-Host': 'motogp2.p.rapidapi.com'
+        },
+        body: JSON.stringify({
+          rider_id: '71df6f0d-51c3-4cdb-9f5c-51939e6f33f2'
+        })
+      };
+
       try {
-        const response = await axios.request(options);
-        setRaces(response.data.races);
+        const response = await fetch(url, options);
+        const data = await response.json();
+        setStatistics(data);
       } catch (error) {
         console.error(error);
       }
-    }
+    };
 
-    fetchRaces();
+    getStatistics();
   }, []);
 
   return (
-
-    
     <div>
-        <div className="NavbarMenu">
-<Navbar>
-    <Nav className="mr-auto">
-        <Nav.Link href="/Home">Home</Nav.Link>
-        <Nav.Link href="/F1">Formula 1</Nav.Link>
-        <Nav.Link href="/MotoGP">MotoGP</Nav.Link>
-    </Nav>
-</Navbar>
-</div>
-      <h1>MotoGP Season</h1>
-      <ul>
-        {races.map((race) => (
-          <li key={race.id}>
-            {race.name} - {race.country} ({race.date})
-          </li>
-        ))}
-      </ul>
+      <div className="NavbarMenu">
+        <Navbar>
+          <Nav className="mr-auto">
+            <Nav.Link href="/Home">Home</Nav.Link>
+            <Nav.Link href="/F1">Formula 1</Nav.Link>
+            <Nav.Link href="/MotoGP">MotoGP</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+      <h1>Rider Statistics</h1>
+      {statistics && statistics.rider && (
+        <div>
+          <p>Name: {statistics.rider.name}</p>
+          <p>Number: {statistics.rider.number}</p>
+          <p>Points: {statistics.points}</p>
+          <p>Wins: {statistics.wins}</p>
+          <p>Podiums: {statistics.podiums}</p>
+        </div>
+      )}
+
     </div>
   );
-}
+};
 
 export default MotoGP;
